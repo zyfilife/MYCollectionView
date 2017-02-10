ee5a5772039f47e8f93598fdba646d732fc9dfaf
朱益锋 <zhuyifeng@zhuyifengdeMacBook-Pro.local>
2017-02-10 16:40:44 +0800

    no message

--- MYCollectionViewDemo/MYCollectionViewDemo/ViewController.swift
+++ MYCollectionViewDemo/MYCollectionViewDemo/ViewController.swift
@@ -8,8 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var array1 = [1,2,3,4]
+    
+    var array2 = [1,2,3,4,5,6]
+    
+    var array3 = [1,2,3]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +28,82 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return self.array1.count
+        case 1:
+            return self.array2.count
+        default:
+            return self.array3.count
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        switch indexPath.section {
+        case 1:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "OutCollectionViewCell", for: indexPath)
+            return cell
+        default:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath)
+            return cell
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionElementKindSectionHeader {
+            let view = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "CollectionViewHeaderView", for: indexPath)
+            return view
+        }else {
+            let view = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionFooter, withReuseIdentifier: "CollectionViewFooterView", for: indexPath)
+            return view
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let totalWidth = collectionView.frame.size.width
+        switch indexPath.section {
+        case 0:
+            let numberOfLines:CGFloat = 4
+            let width = totalWidth/numberOfLines
+            let size = CGSize(width: width, height: width+20)
+            return size
+        case 1:
+            let numberOfLines:CGFloat = 2
+            let width = totalWidth/numberOfLines
+            let size = CGSize(width: width, height: 45)
+            return size
+        default:
+            let numberOfLines:CGFloat = 3
+            let width = totalWidth/numberOfLines
+            let size = CGSize(width: width, height: width+20)
+            return size
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: collectionView.frame.size.width, height: 15)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: collectionView.frame.size.width, height: 41.5)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0000
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0000
+    }
+    
+    
 
 }
 
